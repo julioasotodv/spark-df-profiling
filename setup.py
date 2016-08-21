73b2f7cbe088c23106904477e577b86cb5ae42b4
Julio desktop <julio@esbet.es>
2016-08-21 22:03:20 +0200

    Bumped version to 1.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='spark-df-profiling',
-    version='1.1.4',
+    version='1.1.5',
     author='Julio Antonio Soto de Vicente',
     author_email='julio@esbet.es',
     packages=['spark_df_profiling'],
